a28308446c007e06b50f8a654ff501eaa473e17b
jp <justinbao@hotmail.de>
2015-11-05 16:38:06 +0100

    Tag Entity hinzugefügt

---
+++ src/main/java/com.server/entities/Tag.java
@@ -0,0 +1,52 @@
+package com.server.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ * Created by jp on 05.11.15.
+ */
+@NamedQueries( {
+        @NamedQuery( name = Tag.GET, query = "SELECT t FROM Tag t WHERE t.id = :id" )
+} )
+@Entity
+public class Tag {
+
+    public static final String GET = "Tag.get";
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private int id;
+
+
+    private String name;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public void setId( int id ) {
+        this.id = id;
+    }
+
+
+}
